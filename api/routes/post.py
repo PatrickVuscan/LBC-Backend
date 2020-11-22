313a0225b4e3badc862ea29c6fe09f3d7704eea3
mhd53 <mhd53@users.noreply.github.com>
2020-11-22 15:32:55 -0500

    -chore: linted some stuff

--- api/routes/post.py
+++ api/routes/post.py
@@ -9,6 +9,7 @@ from api.schema.schemas import CreatePost, UpdatePost
 
 router = APIRouter()
 
+
 def get_db():
     session = sessionmaker(engine)
     orm_session = session()
@@ -16,49 +17,53 @@ def get_db():
 
 
 @router.get("/posts/{pid}")
-def get_single_post(pid: int, db: Session=Depends(get_db)):
+def get_single_post(pid: int, dbb: Session = Depends(get_db)):
 
-    post = db.query(UserPosts).filter(UserPosts.post_id == pid).first()
+    post = dbb.query(UserPosts).filter(UserPosts.post_id == pid).first()
 
-    response_body = {'post_id': pid,
-                     'username': post.username,
-                     'anonymous':post.anonymous,
-                     'date_time': post.date_time,
-                     'topic': post.topic,
-                     'post_header': post.post_header,
-                     'post_body': post.post_body,
-                     'comments': post.comments}
+    response_body = {
+        "post_id": pid,
+        "username": post.username,
+        "anonymous": post.anonymous,
+        "date_time": post.date_time,
+        "topic": post.topic,
+        "post_header": post.post_header,
+        "post_body": post.post_body,
+        "comments": post.comments,
+    }
 
     return response_body
 
 
 @router.post("/posts")
-def create_post(request_body: CreatePost, db: Session=Depends(get_db)):
+def create_post(request_body: CreatePost, dbb: Session = Depends(get_db)):
 
-    post_data = {'username': request_body.username,
-            'topic': request_body.topic,
-            'date_time': datetime.now(),
-            'post_header': request_body.post_header,
-            'post_body': request_body.post_body}
+    post_data = {
+        "username": request_body.username,
+        "topic": request_body.topic,
+        "date_time": datetime.now(),
+        "post_header": request_body.post_header,
+        "post_body": request_body.post_body,
+    }
 
     print(post_data)
 
-    db.add(UserPosts(**post_data))
-    db.commit()
+    dbb.add(UserPosts(**post_data))
+    dbb.commit()
 
     return post_data
 
 
 @router.patch("/posts/{pid}")
-def update_post(pid: int, request_body: UpdatePost, db: Session=Depends(get_db)):
+def update_post(pid: int, request_body: UpdatePost, dbb: Session = Depends(get_db)):
 
-    user_post = db.query(UserPosts).filter(UserPosts.post_id == pid).first()
+    user_post = dbb.query(UserPosts).filter(UserPosts.post_id == pid).first()
 
     user_post.anonymous = request_body.anonymous
     user_post.topic = request_body.topic
     user_post.post_header = request_body.post_header
     user_post.post_body = request_body.post_body
 
-    db.commit()
+    dbb.commit()
 
     return request_body
--- api/tests/test_posts.py
+++ api/tests/test_posts.py
@@ -1,24 +1,20 @@
 from unittest import TestCase
-import pytest
 from fastapi.testclient import TestClient
 from app import app
-## import requests
 
 requests = TestClient(app)
-
-base_address = "http://127.0.0.1:5000"
 
 
 class TestPosts(TestCase):
     def test_get_single_post(self):
 
         pid = 1
-        res = requests.get(f"{base_address}/posts/{pid}")
+        res = requests.get(f"/posts/{pid}")
 
         assert res.status_code == 200
         post = res.json()
 
-        assert post["post_id"]  == pid
+        assert post["post_id"] == pid
         assert post["username"] == "Test_Post_Username"
         assert post["anonymous"] is False
         assert post["date_time"] is not None
@@ -31,62 +27,66 @@ class TestPosts(TestCase):
 
     def test_create_post(self):
 
-        request_body = {'username': 'Test_Post_Username',
-                        'anonymous': False,
-                        'topic': "Test_Post_Topic",
-                        'post_header': "Test_Post_Header",
-                        'post_body': "The Sky is Blue."}
+        request_body = {
+            "username": "Test_Post_Username",
+            "anonymous": False,
+            "topic": "Test_Post_Topic",
+            "post_header": "Test_Post_Header",
+            "post_body": "The Sky is Blue.",
+        }
 
-        res = requests.post(f"{base_address}/posts", json=request_body)
+        res = requests.post("/posts", json=request_body)
         post = res.json()
 
         assert res.status_code == 200
         assert post["post_body"] == "The Sky is Blue."
 
-        print('Test Create Post Passed')
-
+        print("Test Create Post Passed")
 
     def test_update_post(self):
-        """ To test updating a post we first need to get the post's data then we update
+        """To test updating a post we first need to get the post's data then we update
         Currently logic is not inplaced for there to be dynamic fields in request body
         so for now we will need to include the fields anonymous, post_header, post_body, topic
         in request body later will make update post request only accept fields which are changing and
         not all these fields every time, but since we do need fields now first send get request for post to update
-        then send a patch request """
+        then send a patch request"""
 
         # Create new post to update so doesn't interfere with old test that specify a specific pid
         # Incase we run the test multiple times
 
-        request_body = {'username': 'Test_Post_Username_2',
-                        'anonymous': False,
-                        'topic': "Test_Post_Topic_2",
-                        'post_header': "Test_Post_Header_2",
-                        'post_body': "The Sky is bright."}
+        request_body = {
+            "username": "Test_Post_Username_2",
+            "anonymous": False,
+            "topic": "Test_Post_Topic_2",
+            "post_header": "Test_Post_Header_2",
+            "post_body": "The Sky is bright.",
+        }
 
-        requests.post(f"{base_address}/posts", json=request_body)
-
+        requests.post("/posts", json=request_body)
 
         pid = 2
-        post_with_id_1 = requests.get(f"{base_address}/posts/{pid}").json()
+        post_with_id_1 = requests.get(f"/posts/{pid}").json()
 
-        request_body = {'anonymous': post_with_id_1['anonymous'],
-                        'post_header': post_with_id_1['post_header'],
-                        'post_body': "Some New Post Body",
-                        'topic': "Another new Post Topic"}
+        request_body = {
+            "anonymous": post_with_id_1["anonymous"],
+            "post_header": post_with_id_1["post_header"],
+            "post_body": "Some New Post Body",
+            "topic": "Another new Post Topic",
+        }
 
-        res_c = requests.patch(f"{base_address}/posts/{pid}", json=request_body)
+        res_c = requests.patch(f"/posts/{pid}", json=request_body)
         assert res_c.status_code == 200
 
-        post_with_id_1_again = requests.get(f"{base_address}/posts/{pid}")
+        post_with_id_1_again = requests.get(f"/posts/{pid}")
         response_body_from_get = post_with_id_1_again.json()
 
-        assert response_body_from_get['post_header'] == post_with_id_1['post_header']
-        assert response_body_from_get['post_body'] == 'Some New Post Body'
+        assert response_body_from_get["post_header"] == post_with_id_1["post_header"]
+        assert response_body_from_get["post_body"] == "Some New Post Body"
 
-        print('Test Update Post')
+        print("Test Update Post")
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     p = TestPosts()
     p.test_create_post()
     p.test_get_single_post()
