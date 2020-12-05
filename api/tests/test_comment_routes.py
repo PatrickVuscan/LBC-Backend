2fe54f968c4d472d16830a5bec5db1931480fcdf
mhd53 <mhd53@users.noreply.github.com>
2020-12-05 15:29:46 -0500

    test: add new comment api route tests (see prev commit)

--- api/tests/test_comment_routes.py
+++ api/tests/test_comment_routes.py
@@ -27,3 +27,44 @@ class TestComments(TestCase):
         assert comment["user_id"] == uid
         assert comment["comment_id"] == cid
         assert comment["content"] == "The Sky is Blue."
+
+    def test_view_n_comments(self):
+        pid = 1
+        uid = 1
+        res1 = client.post(f"/posts/{pid}/comments", json={"content": "The Sky is Blue.", "user_id": uid})
+        res2 = client.post(f"/posts/{pid}/comments", json={"content": "The Sky is Blue.", "user_id": uid})
+        res3 = client.post(f"/posts/{pid}/comments", json={"content": "The Sky is Blue.", "user_id": uid})
+
+        cid1 = res1.json()
+        cid2 = res2.json()
+        cid3 = res3.json()
+
+        res_comments = client.get(f"/posts/{pid}/comments/")
+        assert res_comments.status_code == 200
+
+        comments = res_comments.json()
+        assert comments[0]["comment_id"] == cid3
+        assert comments[1]["comment_id"] == cid2
+        assert comments[2]["comment_id"] == cid1
+
+    def test_view_n_comments_with_offset(self):
+        pid = 1
+        uid = 1
+        res1 = client.post(f"/posts/{pid}/comments", json={"content": "The Sky is Blue.", "user_id": uid})
+        res2 = client.post(f"/posts/{pid}/comments", json={"content": "The Sky is Blue.", "user_id": uid})
+        res3 = client.post(f"/posts/{pid}/comments", json={"content": "The Sky is Blue.", "user_id": uid})
+        client.post(f"/posts/{pid}/comments", json={"content": "The Sky is Blue.", "user_id": uid})
+        client.post(f"/posts/{pid}/comments", json={"content": "The Sky is Blue.", "user_id": uid})
+        client.post(f"/posts/{pid}/comments", json={"content": "The Sky is Blue.", "user_id": uid})
+
+        cid1 = res1.json()
+        cid2 = res2.json()
+        cid3 = res3.json()
+
+        res_comments = client.get(f"/posts/{pid}/comments/?offset=3")
+        assert res_comments.status_code == 200
+
+        comments = res_comments.json()
+        assert comments[0]["comment_id"] == cid3
+        assert comments[1]["comment_id"] == cid2
+        assert comments[2]["comment_id"] == cid1
